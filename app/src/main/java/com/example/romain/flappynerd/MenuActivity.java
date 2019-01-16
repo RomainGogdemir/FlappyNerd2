528065cfe04f6cfb353772f81e4d819c1c6bf15b
PATRY <math3pat@gmail.com>
2019-01-16 11:25:45 +0100

    switch activity from menu to highscore with highscore button in menu

--- app/src/main/java/com/example/romain/flappynerd/MenuActivity.java
+++ app/src/main/java/com/example/romain/flappynerd/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.romain.flappynerd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button highscoreButton = findViewById(R.id.activity_menu_highscore_button);
+        highscoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToHIghscore();
+            }
+        });
+    }
+
+    private void switchToHIghscore() {
+        Intent intent = new Intent(this, HighscoreActivity.class);
+        startActivity(intent);
     }
 }
