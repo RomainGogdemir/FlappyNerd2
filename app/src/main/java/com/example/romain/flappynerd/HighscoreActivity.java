6e950abd05a88dcd7d806dce122a423682b8154f
PATRY <math3pat@gmail.com>
2019-01-16 12:07:54 +0100

    get highscore from shared preferences and display message when no highscore stored

--- app/src/main/java/com/example/romain/flappynerd/HighscoreActivity.java
+++ app/src/main/java/com/example/romain/flappynerd/HighscoreActivity.java
@@ -1,16 +1,41 @@
 package com.example.romain.flappynerd;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HighscoreActivity extends AppCompatActivity {
+
+    public final static String HIGHSCORE = "highscore";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+        displayHighscore();
+    }
+
+    private String[] getHighscores() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String highscoreString = preferences.getString(HIGHSCORE, null);
+        if (highscoreString == null)
+            return null;
+        return highscoreString.split("&");
+    }
+
+    public void displayHighscore(){
+        TextView highscoreDisplay = findViewById(R.id.activity_highscore_textview);
+        String[] highscoreList = getHighscores();
+        if (highscoreList == null){
+            highscoreDisplay.setText(R.string.noHighscore);
+        }
+        else {
+            //highscoreDisplay.setText();&
+        }
     }
 }
